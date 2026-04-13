34f47b0db0d60f6bbca351e49baf227729347563
Prulucas <plbrandsilva@gmail.com>
2026-04-13 11:45:48 -0300

    adding user.models and validation file

--- users/models.py
+++ users/models.py
@@ -1,1 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from core.models import Base
+from .validators import validate_cpf
+import re
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('teacher', 'Professor'),
+        ('student', 'Aluno'),
+    )
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='student'
+    )
+
+    cpf = models.CharField(
+        max_length=11,
+        unique=True,
+        validators=[validate_cpf],
+        null=True,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        if self.cpf:
+            self.cpf = re.sub(r'\D', '', self.cpf)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
---
+++ users/validators.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+from validate_docbr import CPF
+
+
+def validate_cpf(value):
+    cpf = CPF()
+    if not cpf.validate(value):
+        raise ValidationError('CPF inválido')
