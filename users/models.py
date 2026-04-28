590163df8eba0679c150218fd9d600814cf84322
Prulucas <plbrandsilva@gmail.com>
2026-04-28 16:20:51 -0300

    adicionando validação na tela de login e cadastro

--- users/models.py
+++ users/models.py
@@ -25,22 +25,8 @@ class User(AbstractUser):
         ('student', 'Aluno'),
     )
 
-    # ----------------------------------------------
-    # Campo username
-    # Herdado do AbstractUser, mas redefinido
-    # ----------------------------------------------
-    username = models.CharField(
-        max_length=150,
-        unique=True
-    )
-
-    # ----------------------------------------------
-    # Campo e-mail único
-    # Será usado para login
-    # ----------------------------------------------
-    email = models.EmailField(
-        unique=True
-    )
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
 
     # ----------------------------------------------
     # Tipo do usuário
@@ -48,30 +34,16 @@ class User(AbstractUser):
     # student = Aluno
     # ----------------------------------------------
     role = models.CharField(
-        max_length=10,
-        choices=ROLE_CHOICES,
-        default='student'
-    )
+        max_length=10, choices=ROLE_CHOICES, default='student')
 
-    # ----------------------------------------------
-    # CPF do usuário
-    # unique=True  -> não repete
-    # validators   -> valida CPF
-    # blank=True   -> pode ficar vazio no formulário
-    # ----------------------------------------------
-    cpf = models.CharField(
-        max_length=11,
-        unique=True,
-        validators=[validate_cpf],
-        blank=True
-    )
+    cpf = models.CharField(max_length=11, unique=True,
+                           validators=[validate_cpf], blank=True)
 
     # ==================================================
     # LOGIN SERÁ FEITO PELO EMAIL
     # ==================================================
     USERNAME_FIELD = 'email'
 
-    # Campos obrigatórios ao criar superusuário
     REQUIRED_FIELDS = ['username']
 
     # ==================================================
@@ -80,23 +52,12 @@ class User(AbstractUser):
     # ==================================================
     def save(self, *args, **kwargs):
 
-        # Se CPF existir:
         if self.cpf:
-
-            # Remove pontos, traços e caracteres não numéricos
-            # Ex: 111.222.333-44 -> 11122233344
             self.cpf = re.sub(r'\D', '', self.cpf)
 
-        # Define username igual ao email automaticamente
         self.username = self.email
 
-        # Salva normalmente chamando método pai
         super().save(*args, **kwargs)
 
-    # ==================================================
-    # REPRESENTAÇÃO DO OBJETO
-    # ==================================================
     def __str__(self):
-
-        # Mostra email ao imprimir usuário
         return self.email
